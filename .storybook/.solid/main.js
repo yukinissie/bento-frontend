db5c81bd54729c404ec1fdbb51073d000a19b82e
tosaken <v-ntosa@alterbooth.net>
2023-06-03 20:57:55 +0900

    add:各フレームワークのstorybookの実装

---
+++ .storybook/.solid/main.js
@@ -0,0 +1,17 @@
+/** @type { import('storybook-solidjs-vite').StorybookConfig } */
+const config = {
+  stories: ["../../src/components/solid/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "storybook-solidjs-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
