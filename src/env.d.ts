07cab1c2706c2225d48b8b0e757f3d8c30eb6633
K-Kizuku <kotani.kizuku@gmail.com>
2023-06-13 13:32:54 +0900

    [add] 認証用のページの実装

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,19 +1,18 @@
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
-  readonly FIREBASE_PRIVATE_KEY_ID: string;
-  readonly FIREBASE_PRIVATE_KEY: string;
-  readonly FIREBASE_PROJECT_ID: string;
-  readonly FIREBASE_CLIENT_EMAIL: string;
-  readonly FIREBASE_CLIENT_ID: string;
-  readonly FIREBASE_AUTH_URI: string;
-  readonly FIREBASE_TOKEN_URI: string;
-  readonly FIREBASE_AUTH_CERT_URL: string;
-  readonly FIREBASE_CLIENT_CERT_URL: string;
-  readonly FIREBASE_UNIVERSE_DOMAIN: string;
+  readonly PUBLIC_FIREBASE_PRIVATE_KEY_ID: string;
+  readonly PUBLIC_FIREBASE_PRIVATE_KEY: string;
+  readonly PUBLIC_FIREBASE_PROJECT_ID: string;
+  readonly PUBLIC_FIREBASE_CLIENT_EMAIL: string;
+  readonly PUBLIC_FIREBASE_CLIENT_ID: string;
+  readonly PUBLIC_FIREBASE_AUTH_URI: string;
+  readonly PUBLIC_FIREBASE_TOKEN_URI: string;
+  readonly PUBLIC_FIREBASE_AUTH_CERT_URL: string;
+  readonly PUBLIC_FIREBASE_CLIENT_CERT_URL: string;
+  readonly PUBLIC_FIREBASE_UNIVERSE_DOMAIN: string;
   readonly PUBLIC_FIREBASE_API_KEY: string;
   readonly PUBLIC_FIREBASE_AUTH_DOMAIN: string;
-  readonly PUBLIC_FIREBASE_PROJECT_ID: string;
   readonly PUBLIC_FIREBASE_STORAGE_BUCKET: string;
   readonly PUBLIC_FIREBASE_MESSAGEING_SENDER_ID: string;
   readonly PUBLIC_FIREBASE_APP_ID: string;
--- src/utils/firebase/server.ts
+++ src/utils/firebase/server.ts
@@ -3,15 +3,15 @@ import { initializeApp, cert } from "firebase-admin/app";
 
 const serviceAccount = {
   type: "service_account",
-  project_id: import.meta.env.FIREBASE_PROJECT_ID,
-  private_key_id: import.meta.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: import.meta.env.FIREBASE_PRIVATE_KEY,
-  client_email: import.meta.env.FIREBASE_CLIENT_EMAIL,
-  client_id: import.meta.env.FIREBASE_CLIENT_ID,
-  auth_uri: import.meta.env.FIREBASE_AUTH_URI,
-  token_uri: import.meta.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: import.meta.env.FIREBASE_AUTH_CERT_URL,
-  client_x509_cert_url: import.meta.env.FIREBASE_CLIENT_CERT_URL,
+  project_id: import.meta.env.PUBLIC_FIREBASE_PROJECT_ID,
+  private_key_id: import.meta.env.PUBLIC_FIREBASE_PRIVATE_KEY_ID,
+  private_key: import.meta.env.PUBLIC_FIREBASE_PRIVATE_KEY,
+  client_email: import.meta.env.PUBLIC_FIREBASE_CLIENT_EMAIL,
+  client_id: import.meta.env.PUBLIC_FIREBASE_CLIENT_ID,
+  auth_uri: import.meta.env.PUBLIC_FIREBASE_AUTH_URI,
+  token_uri: import.meta.env.PUBLIC_FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: import.meta.env.PUBLIC_FIREBASE_AUTH_CERT_URL,
+  client_x509_cert_url: import.meta.env.PUBLIC_FIREBASE_CLIENT_CERT_URL,
 };
 
 export const app = initializeApp({
