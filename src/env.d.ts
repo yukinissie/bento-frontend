58560cb25b3d101721fde9552e8a68ddc54aa521
K-Kizuku <kotani.kizuku@gmail.com>
2023-06-13 10:13:26 +0900

    [add] firebaseの設定

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,25 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly FIREBASE_PRIVATE_KEY_ID: string;
+  readonly FIREBASE_PRIVATE_KEY: string;
+  readonly FIREBASE_PROJECT_ID: string;
+  readonly FIREBASE_CLIENT_EMAIL: string;
+  readonly FIREBASE_CLIENT_ID: string;
+  readonly FIREBASE_AUTH_URI: string;
+  readonly FIREBASE_TOKEN_URI: string;
+  readonly FIREBASE_AUTH_CERT_URL: string;
+  readonly FIREBASE_CLIENT_CERT_URL: string;
+  readonly FIREBASE_UNIVERSE_DOMAIN: string;
+  readonly PUBLIC_FIREBASE_API_KEY: string;
+  readonly PUBLIC_FIREBASE_AUTH_DOMAIN: string;
+  readonly PUBLIC_FIREBASE_PROJECT_ID: string;
+  readonly PUBLIC_FIREBASE_STORAGE_BUCKET: string;
+  readonly PUBLIC_FIREBASE_MESSAGEING_SENDER_ID: string;
+  readonly PUBLIC_FIREBASE_APP_ID: string;
+  readonly PUBLIC_FIREBASE_MEASUREMENT_ID: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
