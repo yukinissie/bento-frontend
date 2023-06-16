23c1896a9665f6ff34d37d29c8c488b2e0510539
tosaken1116 <nagato20021122@gmail.com>
2023-06-16 19:28:23 +0900

    add:usercardコンポーネントの作成 #8

---
+++ src/components/vue/UserCard/UserCard.stories.js
@@ -0,0 +1,10 @@
+import UserCard from "./UserCard.vue";
+
+export default {
+    component: UserCard,
+    title: "UserCard",
+    tags: ["autodocs"],
+    excludeStories: /.*Data$/,
+};
+
+export const Default = {};
